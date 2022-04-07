5870e08cb644c292534d4c26094fc76a94737244
walkor <walkor@users.noreply.github.com>
2022-04-07 12:38:12 +0800

    Update redis.php

--- src/config/plugin/webman/redis-queue/redis.php
+++ src/config/plugin/webman/redis-queue/redis.php
@@ -3,7 +3,7 @@ return [
     'default' => [
         'host' => 'redis://127.0.0.1:6379',
         'options' => [
-            'auth' => '',         // 密码，可选参数
+            'auth' => null,       // 密码，字符串类型，可选参数
             'db' => 0,            // 数据库
             'max_attempts'  => 5, // 消费失败后，重试次数
             'retry_seconds' => 5, // 重试间隔，单位秒
